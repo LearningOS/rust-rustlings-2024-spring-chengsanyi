85ded2c133ef4ea4358487874491b5f002096aae
Ayu~ <305090753@qq.com>
2024-04-10 12:45:33 -0400

    update

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -10,20 +10,18 @@
 
 
 fn main() {
-    let vec0  = Vec::new();
-
-    println!("{:?} has length {}, with contents: `{:?}`", vec0, vec0.len(), vec0);
-
-
+    let vec0 = Vec::new();
     let mut vec1 = fill_vec(vec0);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
 
-    println!("{:?} has length {}, with contents `{:?}`", vec1, vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,17 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let mut x = 100;
+    
     let y = &mut x;
+    *y += 100;//jie yin yong hou ,ke yi zai ci bei kebian yin yong
     let z = &mut x;
-    *y += 100;
+    
+    
     *z += 1000;
+
     assert_eq!(x, 1200);
 }
