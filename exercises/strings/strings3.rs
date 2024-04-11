3d1351a567e95788f4453938d6aa8ad7e7d2735a
Ayu~ <305090753@qq.com>
2024-04-11 12:01:05 -0400

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,30 +3,21 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    let  (mut i,mut j) = (0,0);
-    for item in input{
-        if item !=b' '{
-            input
-        }else{
-            i+=1;
-        }
-        &input[i..]
-    }
+    input.trim().to_string()
 
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string()+" world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
